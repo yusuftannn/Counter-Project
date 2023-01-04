b5e09ed03fa7eb0dc735b38a7c6e5062e2364f9f
Yusuf Tan <yusuftan41@hotmail.com>
2023-01-04 15:26:27 +0300

    Counter project finished.

---
+++ app.js
@@ -0,0 +1,32 @@
+// set inital count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = "yellow"
+            document.body.style.backgroundColor = "green";
+        }
+        if (count < 0) {
+            value.style.color = "white"
+            document.body.style.backgroundColor = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222"
+            document.body.style.backgroundColor = "#fff";
+        }
+        value.textContent = count;
+    });
+});
